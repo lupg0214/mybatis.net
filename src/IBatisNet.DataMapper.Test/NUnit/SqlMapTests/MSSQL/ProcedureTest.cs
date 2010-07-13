310d0dce503b515081ad50879d55440564b709ec
mmccurrey <mmccurrey@134dda87-75a3-2872-a1b6-3556c44fe488>
2010-07-13 14:03:35 +0000

    Added the ability to specify a command query timeout if you need to extend the command timeout beyond the providers default option. I also took a moment to start the upgrade of the nunit provider to the current version.

--- src/IBatisNet.DataMapper.Test/NUnit/SqlMapTests/MSSQL/ProcedureTest.cs
+++ src/IBatisNet.DataMapper.Test/NUnit/SqlMapTests/MSSQL/ProcedureTest.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using IBatisNet.DataMapper.Test.Domain;
 using NUnit.Framework;
-using NUnit.Framework.SyntaxHelpers;
+
 
 namespace IBatisNet.DataMapper.Test.NUnit.SqlMapTests.MSSQL
 {
