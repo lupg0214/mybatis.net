d55225f8a26e2dfb93cbb690f2542d78cbfff116
mikhaild908@gmail.com <mikhaild908@gmail.com@134dda87-75a3-2872-a1b6-3556c44fe488>
2013-06-10 17:48:37 +0000

    do not create Accounts table if it already exists

--- branches/ibatis-1-maintenance/src/IBatisNet.DataMapper.Test/Scripts/SQLite3/account-init.sql
+++ branches/ibatis-1-maintenance/src/IBatisNet.DataMapper.Test/Scripts/SQLite3/account-init.sql
@@ -1,52 +1,13 @@
--- Creating Table
-
-use [IBatisNet]
-
-if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[Accounts]') and OBJECTPROPERTY(id, N'IsUserTable') = 1)
-BEGIN
-	if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[FK_Orders_Accounts]') and OBJECTPROPERTY(id, N'IsForeignKey') = 1)
-	ALTER TABLE [dbo].[Orders] DROP CONSTRAINT FK_Orders_Accounts
-
-	drop table [dbo].[Accounts]
-END
-
-CREATE TABLE [dbo].[Accounts] (
+CREATE TABLE IF NOT EXISTS Accounts(
 	[Account_ID] [int] NOT NULL ,
 	[Account_FirstName] [varchar] (32)  NOT NULL ,
 	[Account_LastName] [varchar] (32)  NOT NULL ,
 	[Account_Email] [varchar] (128)  NULL,
 	[Account_Banner_Option]  [varchar] (255),
-	[Account_Cart_Option] [int]
-) ON [PRIMARY]
+	[Account_Cart_Option] [int]);
 
-ALTER TABLE [dbo].[Accounts] WITH NOCHECK ADD 
-	CONSTRAINT [PK_Account] PRIMARY KEY  CLUSTERED 
-	(
-		[Account_ID]
-	)  ON [PRIMARY] 
-
--- Creating Test Data
-
-INSERT INTO [dbo].[Accounts] VALUES(1,'Joe', 'Dalton', 'Joe.Dalton@somewhere.com', 'Oui', 200);
-INSERT INTO [dbo].[Accounts] VALUES(2,'Averel', 'Dalton', 'Averel.Dalton@somewhere.com', 'Oui', 200);
-INSERT INTO [dbo].[Accounts] VALUES(3,'William', 'Dalton', null, 'Non', 100);
-INSERT INTO [dbo].[Accounts] VALUES(4,'Jack', 'Dalton', 'Jack.Dalton@somewhere.com', 'Non', 100);
-INSERT INTO [dbo].[Accounts] VALUES(5,'Gilles', 'Bayon', 'gilles.bayon@nospam.org', 'Oui', 100);
-
--- Store procedure
-
-if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[ps_InsertAccount]') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
-drop procedure [dbo].[ps_InsertAccount]
-
-if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[ps_SelectAccount]') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
-drop procedure [dbo].[ps_SelectAccount]
-
-if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[ps_SelectAllAccount]') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
-drop procedure [dbo].[ps_SelectAllAccount]
-
-if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[ps_swap_email_address]') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
-drop procedure [dbo].[ps_swap_email_address]
-
-if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[ps_SelectByIdList]') and 
-OBJECTPROPERTY(id, N'IsProcedure') = 1)
-drop procedure [dbo].[ps_SelectByIdList]
+INSERT INTO [Accounts] VALUES(1,'Joe', 'Dalton', 'Joe.Dalton@somewhere.com', 'Oui', 200);
+INSERT INTO [Accounts] VALUES(2,'Averel', 'Dalton', 'Averel.Dalton@somewhere.com', 'Oui', 200);
+INSERT INTO [Accounts] VALUES(3,'William', 'Dalton', null, 'Non', 100);
+INSERT INTO [Accounts] VALUES(4,'Jack', 'Dalton', 'Jack.Dalton@somewhere.com', 'Non', 100);
+INSERT INTO [Accounts] VALUES(5,'Gilles', 'Bayon', 'gilles.bayon@nospam.org', 'Oui', 100);
