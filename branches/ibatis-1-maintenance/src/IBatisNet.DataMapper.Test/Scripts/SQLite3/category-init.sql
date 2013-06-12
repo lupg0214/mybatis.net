a9f5099b2239a4cd80388b1f9fb98678f381623c
mikhaild908@gmail.com <mikhaild908@gmail.com@134dda87-75a3-2872-a1b6-3556c44fe488>
2013-06-12 20:15:08 +0000

    added maps to project for SQLite3; SQLite3 specific fixes for StatementTest.GenericTestGuidColumn() test;

--- branches/ibatis-1-maintenance/src/IBatisNet.DataMapper.Test/Scripts/SQLite3/category-init.sql
+++ branches/ibatis-1-maintenance/src/IBatisNet.DataMapper.Test/Scripts/SQLite3/category-init.sql
@@ -1,4 +1,6 @@
+DROP TABLE Categories;
+
 CREATE TABLE IF NOT EXISTS Categories(
-	[Category_Id] [int] IDENTITY (1, 1) NOT NULL ,
+	[Category_Id] INTEGER PRIMARY KEY NOT NULL,
 	[Category_Name] [varchar] (32)  NULL,
 	[Category_Guid] [uniqueidentifier] NULL);
