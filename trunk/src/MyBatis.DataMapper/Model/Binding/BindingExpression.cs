a15e009d792fa7ce993b12aafc351b0b43b7f4d4
richard.waspcroft@gmail.com <richard.waspcroft@gmail.com@134dda87-75a3-2872-a1b6-3556c44fe488>
2014-01-16 10:44:23 +0000

    added missing code from check-in (associated with enabling multiple iterate blocks)

---
+++ trunk/src/MyBatis.DataMapper/Model/Binding/BindingExpression.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace MyBatis.DataMapper.Model.Binding
+{
+    internal class BindingExpression
+    {
+        public string FullPropertyName;
+
+        public string Name;
+
+        public string PropertyName;
+
+        public string Value;
+
+        public BindingExpression()
+        {
+        }
+
+        public BindingExpression(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException("name");
+
+            Name = name;
+        }
+    }
+}
