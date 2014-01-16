f077ab68e9ff15be720d220c03e9553beec535c2
richard.waspcroft@gmail.com <richard.waspcroft@gmail.com@134dda87-75a3-2872-a1b6-3556c44fe488>
2014-01-16 10:44:23 +0000

    added missing code from check-in (associated with enabling multiple iterate blocks)

---
+++ src/MyBatis.DataMapper/Model/Binding/BindingReplacement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace MyBatis.DataMapper.Model.Binding
+{
+    internal class BindingReplacement
+    {
+        public string FullPropertyName;
+        public string Name;
+        public string Placeholder;
+        public string Value;
+
+        public void Replace(StringBuilder buffer)
+        {
+            if (buffer == null)
+                throw new ArgumentNullException("buffer");
+
+            var replacementValue = Value;
+
+            if (!String.IsNullOrEmpty(FullPropertyName))
+            {
+                replacementValue = FullPropertyName;
+            }
+
+            buffer.Replace(Placeholder, replacementValue);
+        }
+
+        public void Replace(ref string buffer)
+        {
+            if (buffer == null)
+                throw new ArgumentNullException("buffer");
+
+            var replacementValue = Value;
+
+            if (!String.IsNullOrEmpty(FullPropertyName))
+            {
+                replacementValue = FullPropertyName;
+            }
+
+            buffer = buffer.Replace(Placeholder, replacementValue);
+        }
+    }
+}
